cc49b5cc3108a344aee9b5fdbe2e9f556e20bd5e
Bobby Griffin <robertgriffintm@gmail.com>
2024-02-24 15:24:00 -0500

    add: view ict form to footer

--- views/menu_foot.php
+++ views/menu_foot.php
@@ -3,12 +3,12 @@ $user_id = $user->data()->id;
 ?>
 
     <div class="d-flex justify-content-center flex-lg-row flex-md-row flex-column p-0 ">
-        <a href="<?= $us_url_root?>admin/home.php" class="btn btn-primary m-2">Back to Home Page</a>
+        <a href="<?= $us_url_root?>admin/home.php" class="btn btn-primary m-2">Home Page</a>
         <a href="<?= $us_url_root?>admin/inventory/keg_home.php" class="btn btn-primary m-2">Keg Form</a>
         <a href="<?= $us_url_root?>admin/inventory/coffee_home.php" class="btn btn-primary m-2">Coffee Form</a>
         <a href="<?= $us_url_root?>admin/inventory/syrup_home.php" class="btn btn-primary m-2">Syrup Form</a>
         <a href="<?= $us_url_root?>admin/inventory/pastry_home.php" class="btn btn-primary m-2">Pastry Form</a>
-        <?php if($user_id == 9) { ?>
-        <a href="<?= $us_url_root?>admin/inventory/ict_home.php" class="btn btn-primary m-2">ICT Form</a>
+        <?php if($user_id == 9 || $user_id == 1 || $user_id == 12) { ?>
+            <a href="<?= $us_url_root?>admin/inventory/ict_home.php" class="btn btn-primary m-2">ICT Form</a>
         <?php } ?>
     </div>
