4f7d96870a5930dfa49cfb7cc133b416bab6e31d
Bobby Griffin <robertgriffintm@gmail.com>
2024-05-09 11:02:10 -0400

    change: shops only see their keg inventory

--- admin/inventory/_keg.php
+++ admin/inventory/_keg.php
@@ -2,6 +2,19 @@
 require_once '../../users/init.php';
 require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
 
+
+$user_id = $user->data()->id;
+$store_id = "";
+if($user_id == 9) {
+    $store_id = 1;
+} elseif($user_id == 5) {
+    $store_id = 2;
+} elseif($user_id == 10) {
+    $store_id = 3;
+}
+
+$uname = $user->data()->fname . " " .  $user->data()->lname;
+
 $cb_stock = $db->query("SELECT * FROM inventory_cold_brew_entry WHERE entry_date > CURDATE() - INTERVAL 1 WEEK ORDER BY entry_date DESC")->results();
 // offset date
 
@@ -44,93 +57,167 @@ if($to == "") {
 ?>
 
 
-
-<div class="row my-3">
-    <div class="text-center">
-        <h4 class="mb-2 me-2">View/Hide Inventory:</h4>  
-        <div class="d-flex justify-content-center align-items-center mt-1">   
-            <button id="show1" class="btn btn-info me-2">East End</button>
-            <button id="show2" class="btn btn-warning me-2">Mills</button>
-            <button id="show3" class="btn btn-success me-2">UCF</button>
+<?php // View for Production and Admin Users
+    if($user_id == 3 || $user_id ==1) { ?>
+        <div class="row my-3">
+            <div class="text-center">
+                <h4 class="mb-2 me-2">View/Hide Inventory:</h4>  
+                <div class="d-flex justify-content-center align-items-center mt-1">   
+                    <button id="show1" class="btn btn-info me-2">East End</button>
+                    <button id="show2" class="btn btn-warning me-2">Mills</button>
+                    <button id="show3" class="btn btn-success me-2">UCF</button>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-
-<div class="d-flex flex-column flex-md-row justify-content-evenly mt-4">
-<?php foreach($shop as $s) { ?>
-    <div class="card flex-grow-1 mx-2 my-2"  id="tab<?= $s->id?>">
-        <div class="card-header"><h3 id="shopName<?= $s->id?>"  class=""><?= $s->name ?></h3></div>
-        <div class="card-body">
-            <table class="table table-light table-sm"> 
-                <thead>
-                    <tr>
-                        <th scope="col">Date</th>
-                        <th scope="col" class="">CB Black</th>
-                        <th scope="col" class="">CB White</th>
-                        <th scope="col" class="">CB Vegan</th>
-                    </tr>
-                </thead>
-
-                <tbody class="table-striped mb-3 ">
-                    <?php foreach($cb_stock as $q => $r) { ?>
-                        <?php if ($s->id == $r->store_id) { ?>
-                        <tr class="">
-                            <td class="" scope="" ><?= cleanDate($r->entry_date) ?></td>
-                            <td class=""><?= $r->cbb_stock?></td>
-                            <td class=""><?= $r->cbw_stock?></td>
-                            <td class=""><?= $r->cbv_stock?></td>
+        
+        <?php foreach($shop as $s) { ?>
+            <div class="card flex-grow-1 mx-2 my-2"  id="tab<?= $s->id?>">
+            <div class="card-header"><h4 id="shopName<?= $s->id?>"  class=""><?= $s->name ?></h4></div>
+            <div class="card-body">
+                <table class="table table-light table-sm"> 
+                    <thead>
+                        <tr>
+                            <th scope="col">Date</th>
+                            <th scope="col" class="">CB Black</th>
+                            <th scope="col" class="">CB White</th>
+                            <th scope="col" class="">CB Vegan</th>
                         </tr>
-                    <?php } ?>
-                    <?php } ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
+                    </thead>
+
+                    <tbody class="table-striped mb-3 ">
+                        <?php foreach($cb_stock as $q => $r) { ?>
+                            <?php if ($s->id == $r->store_id) { ?>
+                            <tr class="">
+                                <td class="" scope="" ><?= cleanDate($r->entry_date) ?></td>
+                                <td class=""><?= $r->cbb_stock?></td>
+                                <td class=""><?= $r->cbw_stock?></td>
+                                <td class=""><?= $r->cbv_stock?></td>
+                            </tr>
+                        <?php } ?>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+            </div>
+        <?php } ?>
+<?php } // View for shop users (Mills, East End, UCF)
+        else { ?>
+            <div class="row my-4">
+                <div class="text-center">
+                    <h3><?= $uname ?> Inventory</h3>
+                </div>
+            </div>
+        <?php foreach($shop as $s) { 
+                if($s->id == $store_id) {?>
+                    <div class="card flex-grow-1 mx-2 my-2"  id="tab<?= $s->id?>">
+                        <div class="card-header"><h4 id="shopName<?= $s->id?>"  class=""><?= $s->name ?></h4></div>
+                        <div class="card-body">
+                            <table class="table table-light table-sm"> 
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Date</th>
+                                        <th scope="col" class="">CB Black</th>
+                                        <th scope="col" class="">CB White</th>
+                                        <th scope="col" class="">CB Vegan</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody class="table-striped mb-3 ">
+                                    <?php foreach($cb_stock as $q => $r) { ?>
+                                        <?php if ($s->id == $r->store_id) { ?>
+                                        <tr class="">
+                                            <td class="" scope="" ><?= cleanDate($r->entry_date) ?></td>
+                                            <td class=""><?= $r->cbb_stock?></td>
+                                            <td class=""><?= $r->cbw_stock?></td>
+                                            <td class=""><?= $r->cbv_stock?></td>
+                                        </tr>
+                                    <?php } ?>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                <?php } ?>
+        <?php } ?>
 <?php } ?>
-</div>
 
-
-<div class="row my-3">
-    <div class="text-center">
-        <h4 class="mb-2 me-2">View/Hide Cold Brew Par:</h4>  
-        <div class="d-flex justify-content-center align-items-center mt-1">   
-            <button id="sh1" class="btn btn-info me-2">East End</button>
-            <button id="sh2" class="btn btn-warning me-2">Mills</button>
-            <button id="sh3" class="btn btn-success me-2">UCF</button>
+<?php // View for Production and Admin Users
+    if ($user_id == 3 || $user_id == 1) { ?>
+    <div class="row my-3">
+        <div class="text-center">
+            <h4 class="mb-2 me-2">View/Hide Cold Brew Par:</h4>  
+            <div class="d-flex justify-content-center align-items-center mt-1">   
+                <button id="sh1" class="btn btn-info me-2">East End</button>
+                <button id="sh2" class="btn btn-warning me-2">Mills</button>
+                <button id="sh3" class="btn btn-success me-2">UCF</button>
+            </div>
         </div>
     </div>
-</div>
-<div class="d-flex flex-column flex-md-row justify-content-evenly mt-4">
-<?php foreach($shop as $s) { ?>
-    <div class="card flex-grow-1 mx-2 my-2"  id="partab<?= $s->id?>">
-        <div class="card-header"><h3 id="parName<?= $s->id?>"  class=""><?= $s->name ?></h3></div>
-        <div class="card-body">
-            <table class="table table-sm text-center">
-                <thead class="text-centered">
-                    <tr>
+    <?php foreach($shop as $s) { ?>
+        <div class="card flex-grow-1 mx-2 my-2"  id="partab<?= $s->id?>">
+            <div class="card-header"><h4 id="parName<?= $s->id?>"  class=""><?= $s->name ?></h4></div>
+            <div class="card-body">
+                <table class="table table-sm text-center">
+                    <thead class="text-centered">
+                        <tr>
+                            <?php foreach ($par as $p) { ?>
+                                <?php if($s->id == $p->store_id && ($p->product_id >= 4 && $p->product_id <=6)) { ?>
+                                    <th span="col"><?= $p->product_name ?></th>
+                                <?php } ?>
+                            <?php } ?>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
                         <?php foreach ($par as $p) { ?>
-                            <?php if($s->id == $p->store_id && ($p->product_id >= 4 && $p->product_id <=6)) { ?>
-                                <th span="col"><?= $p->product_name ?></th>
+                        
+                            <?php if($s->id == $p->store_id && ($p->product_id >= 4 && $p->product_id <=6)) {?>
+                                <td><?= $p->par ?></td>
                             <?php } ?>
                         <?php } ?>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <?php foreach ($par as $p) { ?>
-                    
-                        <?php if($s->id == $p->store_id && ($p->product_id >= 4 && $p->product_id <=6)) {?>
-                            <td><?= $p->par ?></td>
-                        <?php } ?>
-                    <?php } ?>
-                    </tr>
-                </tbody>    
-            </table>
+                        </tr>
+                    </tbody>    
+                </table>
+            </div>
+        </div>
+<?php } ?>
+<?php } else { ?>
+    <div class="row my-4">
+        <div class="text-center">
+            <h3><?= $uname ?> Par</h3>
         </div>
     </div>
+    <?php foreach($shop as $s) { 
+        if($s->id == $store_id) {?>
+        <div class="card flex-grow-1 mx-2 my-2"  id="partab<?= $s->id?>">
+            <div class="card-header"><h4 id="parName<?= $s->id?>"  class=""><?= $s->name ?></h4></div>
+            <div class="card-body">
+                <table class="table table-sm text-center">
+                    <thead class="text-centered">
+                        <tr>
+                            <?php foreach ($par as $p) { ?>
+                                <?php if($s->id == $p->store_id && ($p->product_id >= 4 && $p->product_id <=6)) { ?>
+                                    <th span="col"><?= $p->product_name ?></th>
+                                <?php } ?>
+                            <?php } ?>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <?php foreach ($par as $p) { ?>
+                        
+                            <?php if($s->id == $p->store_id && ($p->product_id >= 4 && $p->product_id <=6)) {?>
+                                <td><?= $p->par ?></td>
+                            <?php } ?>
+                        <?php } ?>
+                        </tr>
+                    </tbody>    
+                </table>
+            </div>
+        </div>
+    <?php } ?>
+    <?php } ?>
 <?php } ?>
-</div>
-
 <div class="row mt-4 mb-4">
     <?php require_once $abs_us_root.$us_url_root."views/menu_foot.php" ?>
 </div>
